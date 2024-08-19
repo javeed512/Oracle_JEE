3d4e029447211e072fe56272fe6f01e4daacf7c8
javeed512 <javeedmohammedjaveed@gmail.com>
2024-08-19 17:48:35 +0530

    Http GET,POST methods ,LoginServlet,Welcome file demos , notes, pdf

---
+++ RequestResponseServletDemo/src/main/java/com/oracle/LoginServlet.java
@@ -0,0 +1,61 @@
+package com.oracle;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public LoginServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter  out =	response.getWriter();
+		
+		response.setContentType("text/html");
+		
+		out.print("Login Servlet");
+		
+		
+				String  username =	request.getParameter("username");
+				
+				String password=  request.getParameter("password");
+		
+					
+					out.print("<h1>Welcome "+username+" </h1>");
+					
+					out.print("<h1>Your Password is:  "+password+" </h1>");
+				
+					
+		
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
